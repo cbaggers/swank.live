3133ca3332e4529a1c6cf8bf924aa9d256a4980a
Baggers <chris.bagley@gmail.com>
2016-02-04 21:34:43 +0100

    forgot continuable

--- swank.live.lisp
+++ swank.live.lisp
@@ -1,5 +1,13 @@
 (in-package #:swank.live)
 
+(defmacro continuable (&body body)
+  "Helper macro that we can use to allow us to continue from an
+   error. Remember to hit C in slime or pick the restart so
+   errors don't kill the app."
+  `(restart-case
+       (progn ,@body)
+     (continue () :report "Swank.Live: Continue")))
+
 (defun update-swank ()
   "Called from within the main loop, this keep the lisp repl
    working while cepl runs"
